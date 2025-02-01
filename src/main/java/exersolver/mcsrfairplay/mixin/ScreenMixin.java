7c2b1937bff589598d8ce1fd3ab73963739f9128
contaria <kingcontaria@gmail.com>
2025-02-01 15:08:59 +0100

    cleanup: remove unused Cancellable

--- src/main/java/exersolver/mcsrfairplay/mixin/ScreenMixin.java
+++ src/main/java/exersolver/mcsrfairplay/mixin/ScreenMixin.java
@@ -1,14 +1,12 @@
 package exersolver.mcsrfairplay.mixin;
 
 import com.llamalad7.mixinextras.injector.v2.WrapWithCondition;
-import com.llamalad7.mixinextras.sugar.Cancellable;
 import exersolver.mcsrfairplay.verification_screen.VerificationScreen;
 import net.minecraft.client.gui.screen.Screen;
 import net.minecraft.text.ClickEvent;
 import org.apache.logging.log4j.Logger;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 @Mixin(Screen.class)
 public abstract class ScreenMixin {
@@ -21,7 +19,7 @@ public abstract class ScreenMixin {
                     remap = false
             )
     )
-    private boolean openVerificationScreenClickEvent(Logger logger, String string, Object o, @Cancellable CallbackInfoReturnable<Boolean> cir) {
+    private boolean openVerificationScreenClickEvent(Logger logger, String string, Object o) {
         ClickEvent event = (ClickEvent) o;
         if (event.getAction() == ClickEvent.Action.CHANGE_PAGE && event.getValue().equals("mcsrfairplay.open_verification_screen")) {
             VerificationScreen.start();
